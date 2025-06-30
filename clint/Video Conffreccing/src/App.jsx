393f75beff187037b3e06009fdfce1c56410d609
Guddu kumar <guddukumaryadav10101@gmail.com>
2025-06-30 21:39:16 +0530

    intialize vedio connfrecing app

---
+++ clint/Video Conffreccing/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+import HomePage from './HomePage'
+import LoginPage from './Login'
+
+function App() {
+ return (
+  <>
+
+  <HomePage/>
+  <LoginPage/>
+
+  </>
+ )
+}
+
+export default App
---
+++ clint/Video Conffreccing/src/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div>
+
+<header>
+    <h1>Video Conffreccing</h1>
+    <nav>
+        <h2> GudduVedioApp</h2>
+    </nav>
+</header>
+
+
+    </div>
+  )
+}
